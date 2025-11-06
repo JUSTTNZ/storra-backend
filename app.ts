46c476cdd65c4c99979775d7e3940973ecf82752
JUSTTNZ <chukwudiconfidenceosinachil@gmail.com>
2025-11-06 23:36:24 +0100

    finished setting up the backend

---
+++ app.ts
@@ -0,0 +1,25 @@
+import express from 'express';
+import { errorHandler } from './src/middlewares/ErrorHandler.js'
+const app = express();
+
+// import healthcheckRouter from './src/HealthCheck/healthcheck.route.js';
+// import countryRouter from './src/Country/countryRoute.js'
+// import schoolRouter from './src/School/schoolRoute.js';
+// import studentRouter from './src/Student/studentRoute.js'
+// import individualRouter from './src/Individual/individualRoute.js'
+// import parentRouter from './src/Parent/parentRoute.js'
+import cookieParser from 'cookie-parser';
+
+app.use(express.json({limit: "16kb"}));
+app.use(express.urlencoded({ extended: true, limit: '16kb' }));
+app.use(cookieParser());
+
+// Health check route
+// app.use("/api/v1", healthcheckRouter);
+// app.use("/api/v1/country", countryRouter);
+// app.use("/api/v1/school", schoolRouter);
+// app.use("/api/v1/student", studentRouter)
+// app.use("/api/v1/individual", individualRouter)
+// app.use("/api/v1/parent", parentRouter)
+app.use( errorHandler )
+export { app };
---
+++ index.ts
@@ -0,0 +1,61 @@
+import dotenv from "dotenv";
+import { app } from "./app.js";
+import connectDB from "./src/config/db/db.js";
+// import { connectRedis} from './src/config/redis/redis';
+// import { initCountryCounter } from './src/config/redis/redisCountryCounter';
+// import { initSchoolCounter } from './src/config/redis/redisSchoolCounter';
+// import { initStudentCounter } from './src/config/redis/redisStudentCounter';
+// import { initIndividualCounter, checkCounterHealth } from './src/config/redis/redisIndividualCounter'
+// import { initChildCounter } from "./src/config/redis/redisChildCounter";
+import { logger } from "./src/utils/logger.js";
+import { fileURLToPath } from "url";
+import { dirname, join } from "path";
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
+dotenv.config({path: join(__dirname, ".env")});
+
+const PORT = process.env.PORT || 7001;
+
+
+const startup = async () => {
+  try {
+
+    await connectDB();
+    logger.info('✅ MongoDB connected');
+
+    // await connectRedis();
+    // logger.info('✅ Redis connected');
+
+    // await initCountryCounter();
+    // logger.info('✅ Country counter initialized');
+
+    // await initSchoolCounter();
+    // logger.info('✅ School counter initialized');
+
+    // await initStudentCounter();
+    // logger.info('✅ Student counter initialized');
+
+    // await checkCounterHealth();
+    
+    // await initIndividualCounter();
+    // logger.info('✅ Individual counter initialized')
+
+    // await initChildCounter();
+    // logger.info('✅ Individual counter initialized')
+
+    app.listen(PORT, () => {
+      logger.info(`🚀 Server running on port ${PORT}`);
+    });
+  
+
+  } catch (error) {
+    logger.error('🔥 Startup failed:', {
+      error: error.message,
+      stack: error.stack
+    });
+    process.exit(1);
+  }
+};
+
+startup();
